2f7b385e5c2bf883250c2b5ce37d834ef4b76307
Giovanni Iapaolo <giovanniiapaolo6@gmail.com>
2022-03-02 10:39:39 +0100

    commit in "main

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -17,8 +17,8 @@ const devWebpackConfig = merge(baseWebpackConfig, {
 	module: {
 		rules: utils.styleLoaders({
 			sourceMap: config.dev.cssSourceMap,
-			usePostCSS: true
-		})
+			usePostCSS: true,
+		}),
 	},
 	// cheap-module-eval-source-map is faster for development
 	devtool: config.dev.devtool,
@@ -33,9 +33,9 @@ const devWebpackConfig = merge(baseWebpackConfig, {
 					to: path.posix.join(
 						config.dev.assetsPublicPath,
 						"index.html"
-					)
-				}
-			]
+					),
+				},
+			],
 		},
 		hot: true,
 		contentBase: false, // since we use CopyWebpackPlugin.
@@ -50,12 +50,12 @@ const devWebpackConfig = merge(baseWebpackConfig, {
 		proxy: config.dev.proxyTable,
 		quiet: true, // necessary for FriendlyErrorsPlugin
 		watchOptions: {
-			poll: config.dev.poll
-		}
+			poll: config.dev.poll,
+		},
 	},
 	plugins: [
 		new webpack.DefinePlugin({
-			"process.env": require("../config/dev.env")
+			"process.env": require("../config/dev.env"),
 		}),
 		new webpack.HotModuleReplacementPlugin(),
 		new webpack.NamedModulesPlugin(), // HMR shows correct file names in console on update.
@@ -64,17 +64,17 @@ const devWebpackConfig = merge(baseWebpackConfig, {
 		new HtmlWebpackPlugin({
 			filename: "index.html",
 			template: "index.html",
-			inject: true
+			inject: true,
 		}),
 		// copy custom static assets
 		new CopyWebpackPlugin([
 			{
 				from: path.resolve(__dirname, "../static"),
 				to: config.dev.assetsSubDirectory,
-				ignore: [".*"]
-			}
-		])
-	]
+				ignore: [".*"],
+			},
+		]),
+	],
 });
 
 module.exports = new Promise((resolve, reject) => {
@@ -93,12 +93,12 @@ module.exports = new Promise((resolve, reject) => {
 				new FriendlyErrorsPlugin({
 					compilationSuccessInfo: {
 						messages: [
-							`Your application is running here: http://${devWebpackConfig.devServer.host}:${port}`
-						]
+							`Your application is running here: http://${devWebpackConfig.devServer.host}:${port}`,
+						],
 					},
 					onErrors: config.dev.notifyOnErrors
 						? utils.createNotifierCallback()
-						: undefined
+						: undefined,
 				})
 			);
 
